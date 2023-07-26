67494031f378481d2c816b84bd1857ecee9ab3ab
agusvd <agusvilladirstine@gmail.com>
2023-07-26 17:25:32 -0300

    nueva update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
                             </div>
                         </motion.div>
                     </div>
-                    <div className='mt-16'>
+                    <div className='mt-16 border-xl'>
                     {/* Mapa */}
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2392.023364725136!2d-70.91014878416841!3d-53.163620079941104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb26344f9de9f9b%3A0x341656d2cc37809a!2sNotar%C3%ADa%20Alfredo%20Fonseca%20Mihovilovic!5e0!3m2!1ses!2scl!4v1686708410707!5m2!1ses!2scl"
