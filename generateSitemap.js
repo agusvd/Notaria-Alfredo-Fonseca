3ff97c92ecf29bafd118ecab117cc60cad5cbfc6
agusvd <agusvilladirstine@gmail.com>
2023-08-31 18:06:11 -0300

    fix detalles

--- generateSitemap.js
+++ generateSitemap.js
@@ -1,17 +1,22 @@
-const Sitemap = require('sitemap');
-const fs = require('fs');
+import { SitemapStream, streamToPromise } from 'sitemap';
 
-const sitemap = Sitemap.createSitemap({
-    hostname: 'https://www.notariafonseca.cl', // Reemplaza con tu dominio
-    urls: [
-        { url: '/', changefreq: 'weekly', priority: 0.8 },
-        { url: '/', changefreq: 'weekly', priority: 0.7 },
-        { url: '/Notaria-express', changefreq: 'weekly', priority: 0.6 },
-        { url: '/Contacto', changefreq: 'weekly', priority: 0.5 },
-        { url: '/Nosotros', changefreq: 'weekly', priority: 0.4 },
-        // Agrega más URLs aquí
-    ]
+import fs from 'fs';
+
+const sitemapStream = new SitemapStream({
+    hostname: 'https://www.notariafonseca.cl' // Reemplaza con tu dominio
 });
 
+
+sitemapStream.write({ url: '/', changefreq: 'weekly', priority: 0.8 });
+sitemapStream.write({ url: '/pagina1', changefreq: 'weekly', priority: 0.7 });
+sitemapStream.write({ url: '/Notaria-express', changefreq: 'weekly', priority: 0.6 });
+sitemapStream.write({ url: '/Contacto', changefreq: 'weekly', priority: 0.5 });
+sitemapStream.write({ url: '/Nosotros', changefreq: 'weekly', priority: 0.4 });
+
+
+sitemapStream.end();
+
 // Guardar la sitemap en un archivo
-fs.writeFileSync('./public/sitemap.xml', sitemap.toString());
+streamToPromise(sitemapStream).then((data) => {
+    fs.writeFileSync('./public/sitemap.xml', data.toString());
+});
