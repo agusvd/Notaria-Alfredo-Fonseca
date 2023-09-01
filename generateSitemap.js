e06b821234901956ee701bfd4b659d0d230cdabe
agusvd <agusvilladirstine@gmail.com>
2023-09-01 08:38:22 -0300

    fix detalles

--- generateSitemap.js
+++ generateSitemap.js
@@ -6,12 +6,13 @@ const sitemapStream = new SitemapStream({
     hostname: 'https://www.notariafonseca.cl' // Reemplaza con tu dominio
 });
 
-
 sitemapStream.write({ url: '/', changefreq: 'weekly', priority: 0.8 });
-sitemapStream.write({ url: '/Notaria-express', changefreq: 'weekly', priority: 0.6 });
-sitemapStream.write({ url: '/Contacto', changefreq: 'weekly', priority: 0.5 });
-sitemapStream.write({ url: '/Nosotros', changefreq: 'weekly', priority: 0.4 });
 
+sitemapStream.write({ url: '/Notaria-express', changefreq: 'weekly', priority: 0.6 });
+
+sitemapStream.write({ url: '/Contacto', changefreq: 'weekly', priority: 0.5 });
+
+sitemapStream.write({ url: '/Nosotros', changefreq: 'weekly', priority: 0.4 });
 
 sitemapStream.end();
 
